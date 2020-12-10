5a4f678c1befed0d085ee1bae1a04fde5e89653b
raulstvz <estevezterrados@gmail.com>
2020-12-10 21:03:47 +0100

    Dec 10

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ const { json, urlencoded } = require('body-parser');
 const morgan = require('morgan');
 const config = require('./config.js');
 const cors = require('cors');
+const jwt = require('express-jwt');
+const jsonwebtoken = require('jsonwebtoken');
+
+const persimon = require('./utils/persimon');
+const db = persimon('/assets/users.json');
 
 const app = express();
 
@@ -23,6 +28,41 @@ app.use('/api/users', usersRouter);
 
 const start = async () => {
   try {
+    const secretToken = 'picturest';
+
+    app.post('/login', (req, res) => {
+      //validate user: email, password using db.all();
+
+      const { email, password } = req.body;
+
+      const users = db.all();
+
+      const user = users.find((user) => {
+        return user.email === email && user.password === password;
+      });
+
+      if (user) {
+        const accesToken = jsonwebtoken.sign(
+          {
+            email: user.email,
+            password: user.password,
+          },
+          secretToken
+        );
+        res.send(accesToken);
+      } else {
+        res.send('Email or password incorrect');
+      }
+    });
+
+    app.get(
+      '/protected',
+      jwt({ secret: secretToken, algorithms: ['HS256'] }),
+      (req, res) => {
+        res.send('protected');
+      }
+    );
+
     app.listen(config.port, () => {
       console.log(`REST API on http://localhost:${config.port}`);
     });
