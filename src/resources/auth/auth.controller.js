100abea7b387fd9be3e8285f3bdab136ec4044e9
raulstvz <estevezterrados@gmail.com>
2021-02-01 19:22:20 +0100

    tests

--- src/resources/auth/auth.controller.js
+++ src/resources/auth/auth.controller.js
@@ -1,19 +1,20 @@
 const userModel = require('../users/users.model');
 const jwt = require('jsonwebtoken');
 
-const login = async(req, res) => {
-    const { email, password } = req.body;
-    const user = await userModel.getByEmail(email);
-    if (user.password === password)
-    {
-      const token = jwt.sign({email: email, role: 'admin'}, process.env.TOKEN_SECRET);
-      res.json(token);
-    } 
-    else {
-      res.status(401).send("Username or password incorrect");
-    }
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.getByEmail(email);
+  if (user.password === password) {
+    const token = jwt.sign({ email: email, role: 'admin' }, process.env.TOKEN_SECRET);
+    res.json({
+      token: token,
+      user: user,
+    });
+  }
+  else {
+    res.status(401).send("Username or password incorrect");
+  }
 };
-
 module.exports = {
   login
 };
